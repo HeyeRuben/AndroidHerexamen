6e0f61336f20354ae521a6d9c7433e54f75d2f6c
HeyeRuben <47556289+HeyeRuben@users.noreply.github.com>
2022-08-08 02:18:34 +0200

    [NEW] Added LiveData List of posts in MainViewModel.kt (strings)

--- app/src/main/java/com/example/androidherexamen/MainViewModel.kt
+++ app/src/main/java/com/example/androidherexamen/MainViewModel.kt
@@ -1,7 +1,18 @@
 package com.example.androidherexamen
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel(){
+
+    // Bevat de lijst met posts (voorbeeld lijst van tekst)
+
+    private lateinit var list: MutableLiveData<MutableList<String>>
+
+    init {
+        list.value?.add("test1");
+        list.value?.add("test2");
+        list.value?.add("test3");
+    }
 
 }
