c8e2da9716b1e00a7042fd9a56f7279f3e1506d7
HeyeRuben <47556289+HeyeRuben@users.noreply.github.com>
2022-08-10 16:28:44 +0200

    [FIX] Display posts list in fragment_main.xml

--- app/src/main/java/com/example/androidherexamen/main/MainViewModel.kt
+++ app/src/main/java/com/example/androidherexamen/main/MainViewModel.kt
@@ -1,27 +1,25 @@
 package com.example.androidherexamen.main
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.example.androidherexamen.database.Post
 import com.example.androidherexamen.database.PostDatabaseDAO
+import kotlinx.coroutines.launch
 
 class MainViewModel(userId: Int, val database: PostDatabaseDAO, application: Application) : AndroidViewModel(application){
 
-    // Bevat de lijst met posts (voorbeeld lijst van tekst)
-
-    //Private attribute (Mutable Live Data)
-    private val _list : MutableLiveData<MutableList<String>> = MutableLiveData(mutableListOf("test1", "test2", "test3"))
-
-    //public attribute (Live Data
-    val list : LiveData<MutableList<String>>
-        get() = _list
+    // Bevat de lijst met posts
+    val posts = database.getAll() // Impl: via string formatter de data weergeven
 
     fun updateList(){
-        _list.value!!.add("new item")
-        var list : MutableLiveData<MutableList<String>> = _list
+        viewModelScope.launch{
+            val newPost = Post()
+            //newPost.text = kotlin.random.Random.nextInt().toString()
+            insert(newPost)
+        }
+    }
 
-        _list.value = list.value
+    private suspend fun insert(newPost: Post) {
+        database.insert(newPost)
     }
 }
