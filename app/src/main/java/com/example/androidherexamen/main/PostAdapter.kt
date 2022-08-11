59bf8b93ba386c1699ee5b93cf702c6a5036cdf3
HeyeRuben <47556289+HeyeRuben@users.noreply.github.com>
2022-08-11 14:39:11 +0200

    [UPD] Refactor PostAdapter.kt with cleaner code and encapsulation

--- app/src/main/java/com/example/androidherexamen/main/PostAdapter.kt
+++ app/src/main/java/com/example/androidherexamen/main/PostAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.androidherexamen.main
 
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,7 +9,6 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidherexamen.R
 import com.example.androidherexamen.database.Post
-import org.w3c.dom.Text
 
 class PostAdapter : RecyclerView.Adapter<PostAdapter.ViewHolder>(){
 
@@ -24,29 +24,37 @@ class PostAdapter : RecyclerView.Adapter<PostAdapter.ViewHolder>(){
         // Alle velden die zichtbaar zijn worden hergebruikt dus in deze methode
         // Moet alles ingesteld worden
         val item = data[position]
-
-        // Hier alle gegevens uit item halen bv item. momenteel nog hardcoded voor testing
-        holder.date.text = "11/08/2022"
-        holder.postText.text = item.text
-        // holder.image nog op te zoeken
-        holder.links.text = "Hier kunnen links verschijnen"
+        holder.bind(item)
     }
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.post_view, parent, false)
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    class ViewHolder private constructor(itemView: View): RecyclerView.ViewHolder(itemView){
 
         val image: ImageView = itemView.findViewById(R.id.post_image)
         val postText: TextView = itemView.findViewById(R.id.post_text)
         val date: TextView = itemView.findViewById(R.id.post_date)
         val links: TextView = itemView.findViewById(R.id.post_links)
 
+        fun bind(item: Post) {
+
+            // Hier alle gegevens uit item halen bv item. momenteel nog hardcoded voor testing
+            date.text = "11/08/2022"
+            postText.text = item.text
+            // holder.image nog op te zoeken
+            links.text = "Hier kunnen links verschijnen"
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.post_view, parent, false)
+                return ViewHolder(view)
+            }
+        }
+
     }
-
-
-
 }
