7e26c3c16ab1997eca9f5d0bd420c942160530b1
HeyeRuben <47556289+HeyeRuben@users.noreply.github.com>
2022-08-11 23:50:23 +0200

    [NEW] Implemented OnClickListener to MainFragment.kt (comments button)

--- app/src/main/java/com/example/androidherexamen/main/MainFragment.kt
+++ app/src/main/java/com/example/androidherexamen/main/MainFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -38,7 +39,9 @@ class MainFragment : Fragment() {
 
         binding.mainViewModel = mainViewModel
 
-        val adapter = PostAdapter()
+        val adapter = PostAdapter(PostCommentsListener {
+            postId -> println(postId.toString())
+        })
         binding.postsList.adapter = adapter
 
         mainViewModel.posts.observe(viewLifecycleOwner, Observer {
