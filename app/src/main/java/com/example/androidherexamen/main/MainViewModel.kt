1fcaaaa97f807acbb466339fb64ddc2496288302
HeyeRuben <47556289+HeyeRuben@users.noreply.github.com>
2022-08-12 18:29:14 +0200

    [FIX] Prevent null object reference being deleted

--- app/src/main/java/com/example/androidherexamen/main/MainViewModel.kt
+++ app/src/main/java/com/example/androidherexamen/main/MainViewModel.kt
@@ -24,6 +24,7 @@ class MainViewModel(userId: Int, val database: PostDatabaseDAO, application: App
     }
 
     private suspend fun delete(post: Post){
+        if(post != null)
         database.delete(post)
     }
 
