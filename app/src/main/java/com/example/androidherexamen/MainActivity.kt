8929a7c745f5749230b163a5626e9f97886de30b
HeyeRuben <47556289+HeyeRuben@users.noreply.github.com>
2022-08-13 23:19:37 +0200

    [UPD] MainActivity.kt uses AppCompatActivity

--- app/src/main/java/com/example/androidherexamen/MainActivity.kt
+++ app/src/main/java/com/example/androidherexamen/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.androidherexamen
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.androidherexamen.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : FragmentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
