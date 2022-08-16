f182d3b45b92a15b6ddfd45d263ce0a71adeceb4
HeyeRuben <47556289+HeyeRuben@users.noreply.github.com>
2022-08-16 12:08:01 +0200

    [NEW] Added posts overview to begeleider dashboard

--- app/src/main/java/com/example/androidherexamen/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/androidherexamen/dashboard/DashboardFragment.kt
@@ -6,25 +6,61 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.androidherexamen.R
+import com.example.androidherexamen.database.MyDatabase
 import com.example.androidherexamen.databinding.FragmentDashboardBinding
 import com.example.androidherexamen.databinding.FragmentMainBinding
+import com.example.androidherexamen.main.*
 
 class DashboardFragment : Fragment() {
-
-    private lateinit var viewModel : DashboardViewModel
-
-    private lateinit var binding : FragmentDashboardBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //Inflate view + instance van binding klasse
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_dashboard, container, false)
 
-        viewModel = ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+        val binding: FragmentDashboardBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_dashboard, container, false)
+
+        val application = requireNotNull(this.activity).application
+
+        val dataSource = MyDatabase.getInstance(application).postDatabaseDAO
+
+        val viewModelFactory = DashboardViewModelFactory(1, dataSource, application)
+
+        val dashboardViewModel = ViewModelProvider(this, viewModelFactory).get(DashboardViewModel::class.java)
+
+        binding.dashboardViewModel = dashboardViewModel
+
+        val adapter = PostAdapter(PostCommentsListener {
+                postId -> dashboardViewModel.onCommentsClicked(postId)
+        }, DeletePostListener {
+                postId -> dashboardViewModel.onDeletePostClicked(postId)
+        }, AddPostToFavoritesClickListener {
+                postId -> dashboardViewModel.onFavoritePostClicked(postId)
+        })
+
+        binding.dashboardPostsList.adapter = adapter
+
+        dashboardViewModel.navigateToComments.observe(this, Observer {post ->
+            post?.let {
+                this.findNavController().navigate(
+                    DashboardFragmentDirections
+                        .actionDashboardToCommentsFragment(post))
+                dashboardViewModel.onCommentsNavigated()
+            }
+        })
+
+        dashboardViewModel.posts.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.submitList(it)
+            }
+        })
+
+        binding.lifecycleOwner = this
 
         return binding.root
     }
--- app/src/main/java/com/example/androidherexamen/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/androidherexamen/dashboard/DashboardViewModel.kt
@@ -1,7 +1,36 @@
 package com.example.androidherexamen.dashboard
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.androidherexamen.database.PostDatabaseDAO
+import kotlinx.coroutines.launch
 
-class DashboardViewModel : ViewModel(){
+class DashboardViewModel(userId: Int, val database: PostDatabaseDAO, application: Application) : AndroidViewModel(application){
+
+    // Bevat de lijst met posts
+    val posts = database.getAll() // Impl: via string formatter de data weergeven
+
+    private val _navigateToComments = MutableLiveData<Long?>()
+    val navigateToComments
+        get() = _navigateToComments
+
+    fun onCommentsClicked(postId: Long){
+        _navigateToComments.value = postId
+    }
+
+    fun onDeletePostClicked(postId: Long){
+        // mag dit niet doen
+    }
+
+    fun onFavoritePostClicked(postId: Long){
+        // mag dit niet doen
+    }
+
+    fun onCommentsNavigated() {
+        _navigateToComments.value = null
+    }
 
 }
